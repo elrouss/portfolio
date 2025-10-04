03c5e17a4708dd0d8943ed6ba77a99e8aa883a99
Boris Zashliapin <test-zashliapin-b@yandex.ru>
2025-10-04 14:45:14 +0300

    feat: implementate modals

--- portfolio/src/index.js
+++ portfolio/src/index.js
@@ -3,6 +3,8 @@ import * as icons from './blocks/icon/icon.js';
 import Auth from './services/auth.js';
 
 import Header from './blocks/header/header.js';
+
+import PricingCardList from './blocks/pricing-card-list/pricing-card-list.js';
 import Accordion from './blocks/accordion/accordion.js';
 
 void icons;
@@ -10,4 +12,9 @@ void icons;
 Auth.authorize();
 
 new Header(document.querySelector('.header'));
+
+new PricingCardList(
+  document.querySelector('.pricing-card-list'),
+  document.querySelector('.dialog')
+);
 new Accordion(document.querySelector('.faq__accordion'));
